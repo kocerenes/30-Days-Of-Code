23d24a814343285f2d8389017d9a8b30f6aeb052
Enes Koçer <57600757+kocerenes@users.noreply.github.com>
2022-02-03 13:27:14 +0300

    Add files via upload

---
+++ Day 7 Arrays.cpp
@@ -0,0 +1,26 @@
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+    
+    for(auto it = arr.rbegin(); it != arr.rend(); it++){
+        cout << *it << " ";
+    }
+
+    return 0;
+}
